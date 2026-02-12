36ce9b27192e44f98c5ef1b5d304cceb5a8f4a95
Lorena <230255@fppiramide.com>
2026-02-12 12:26:49 +0100

    cosas

--- src/tareas/infraestructure/db/tareas.repository.postgre.ts
+++ src/tareas/infraestructure/db/tareas.repository.postgre.ts
@@ -25,10 +25,26 @@ export default class TareasRepositoryPostgres implements TareasRepository {
     }
 
     async createTask(tarea: Tarea): Promise<Tarea> {
-        const query = "INSERT INTO tareas (usuario_id, texto, prioridad, fecha_creacion, fecha_finalizacion, estado) VALUES ('${tarea.usuario.id}', '${tarea.texto}', '${tarea.prioridad}', now(), '${tarea.fecha_finalizacion}', '${tarea.estado}'";
+        const query = `INSERT INTO tareas (usuario_id, texto, prioridad, fecha_creacion, fecha_finalizacion, estado) VALUES ('${tarea.usuario.id}', '${tarea.texto}', '${tarea.prioridad}', now(), '${tarea.fecha_finalizacion}', '${tarea.estado}'`;
         const results: any = await executeQuery(query);
         tarea.id = results[0].id;
         tarea.fechaCreacion = results[0].fecha_creacion;
         return tarea;
+    }
+
+    async changeState(tarea: Tarea): Promise<Tarea> {
+        const query = `UPDATE tareas SET estado = '${tarea.estado}' WHERE id = '${tarea.id}'`;
+        await executeQuery(query);
+        return tarea;
+    }
+
+    async updateTask(tarea: Tarea): Promise<Tarea> {
+        const query = `UPDATE tareas SET (texto, prioridad, estado) values ('${tarea.texto}', '${tarea.prioridad}', '${tarea.estado}') WHERE id = '${tarea.id}'`;
+        await executeQuery(query);
+        return tarea;
+    }
+
+    async assignTask(tarea: Tarea, asignado: Usuario): Promise<Tarea> {
+        const query = `INSERT INTO tareas ()`
     }
 }
